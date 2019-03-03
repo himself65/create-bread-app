45c65a4a6f512ffb6cc8ea16ffaa6587b1ceb8ae
himself65 <himself6565@gmail.com>
2019-03-03 08:46:08 +0800

    init repo

---
+++ index.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk')
+const currentNodeVersion = process.versions.node
+const semver = currentNodeVersion.split('.')
+const major = semver[0]
+
+if (major < 8) {
+  console.error(
+    chalk.red(
+      'You are running Node ' +
+      currentNodeVersion +
+      '.\n' +
+      'Create Bread App requires Node 8 or higher. \n' +
+      'Please update your version of Node.'
+    )
+  )
+  process.exit(1)
+}
+
+require('./lib/creat-bread-app')
