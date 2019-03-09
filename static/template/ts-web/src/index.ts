905d29722aba4106d60e42bfbe4791396f10f061
himself65 <himself6565@gmail.com>
2019-03-09 13:06:41 +0800

    fix

--- static/template/ts-web/src/index.ts
+++ static/template/ts-web/src/index.ts
@@ -1,5 +1,16 @@
 import './main.styl'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const app = document.getElementById('app') || document.createElement('div')
+  app.innerHTML = `
+  <div>
+    <span>hello, world</span>
+    <br/>
+    <a href="https://github.com/himself65">Himself65 Github</a>
+  </div>
+  `
+})
+
+document.addEventListener('loadend', () => {
   console.log('ts-web loaded.')
 })
