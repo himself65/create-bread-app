c4f97cd680bfa85af061ab51b100e21c5f00f5d8
himself65 <himself6565@gmail.com>
2019-03-10 10:26:10 +0800

    support copy files

---
+++ static/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-pipeline-operator',
+    '@babel/plugin-proposal-function-bind',
+    '@babel/plugin-syntax-dynamic-import'
+  ]
+}
