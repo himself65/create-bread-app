3c47d5cde530bc1edc964c158bb1e748ece562fb
himself65 <himself6565@gmail.com>
2019-03-10 10:52:49 +0800

    fix

--- static/babel.config.js
+++ static/babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-pipeline-operator',
+    ['@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' }],
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-function-bind',
     '@babel/plugin-syntax-dynamic-import'
